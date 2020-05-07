300af11c99c316cfaa31ddf720137ac3825b8019
Miguel Cosenza <migueljcc5@gmail.com>
2020-05-07 16:18:49 +0200

    first draft

---
+++ protein_coverage_visualization.R
@@ -0,0 +1,44 @@
+## Protein coverage visualization script ----
+# Miguel Cosenza 07.05.2020 ----
+
+## WHICH PROTEIN DO YOU WANT TO PLOT? ----
+# please type the UNIPROT ID for the desired protein
+
+proteinid <- "Q5JWB9"
+
+## EXECUTE CODE ----
+
+# Load required data ----
+if(!exists("pepts")){
+pepts <- read.delim(file = "Data/peptides.txt",
+                       sep = "\t",
+                       stringsAsFactors = FALSE)} 
+
+if(!exists("proteins")){
+proteins <- read.delim(file = "Data/proteinGroups.txt",
+                       sep = "\t",
+                       stringsAsFactors = FALSE)}
+
+# Generate data frame for plotting ----
+
+source(here::here("R/getcovaragedata.R"))
+
+covdata <- getcoveragedata(proteinGroups = proteins,
+                           peptides = pepts)
+
+# Generate peptide-to-protein coverage plot ----
+
+source(here::here("R/plot_coverage.R"))
+
+plot_coverage(covdata,
+              id = proteinid)
+
+covplot <- plot_coverage(covdata,id = proteinid)
+
+# Generate image file ----
+
+if(!dir.exists("Plot_outputs"))dir.create("Plot_outputs")
+
+ggsave(filename = paste0("Plot_outputs/coverage_plot_",proteinid,".tiff"),plot = covplot,
+       device = 'tiff')
+
